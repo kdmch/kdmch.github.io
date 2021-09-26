4eccf747dcd903fa192722b7dd1b353ebbb8d0b9
kdmch <mitori.kadomachi@gmail.com>
2021-09-26 19:03:57 +0900

    touch

--- p5test/sketch.js
+++ p5test/sketch.js
@@ -1,3 +1,5 @@
+var pos = 0;
+var dy, sy, moveflag, touchflag = 0;
 
 function setup(){
     createCanvas(window.innerWidth,window.innerHeight);
@@ -9,9 +11,36 @@ function draw(){
     fill(0,0,0);
     rect(0,0,width,height);
     fill(0,128,0);
-    ellipse(width/2,height/2,100);
+    ellipse(width/2,height/2+pos,100);
 }
 
 function windowResized() {
     resizeCanvas(window.innerWidth,window.innerHeight);
+}
+
+function mouseWheel(event) {
+    dy = event.delta;
+    print(dy);
+    pos += dy;
+    return false;
+}
+
+function touchStarted() {
+    dy = 0;
+    sy = mouseY;
+    moveflag = false;
+    touchflag = true;
+    return false;
+}
+
+function touchMoved() {
+    dy = sy - mouseY;
+    sy = mouseY;
+    pos += dy;
+    return false;
+}
+
+function touchEnded() {
+    touchflag = false;
+    return false;
 }
