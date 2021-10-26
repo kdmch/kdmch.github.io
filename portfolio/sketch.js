7be87c0dcad4b0c21bab1660bb4f8e9ccfe1afca
kdmch <mitori.kadomachi@gmail.com>
2021-10-27 02:09:47 +0900

    imagetile

--- portfolio/sketch.js
+++ portfolio/sketch.js
@@ -1,26 +1,29 @@
-let img;
-let imgurlt = "./img/cover.jpg";
+let img = [];
+let imgdscr = [];
+imgdscr[0] = 'icon';
 
-function preload() {
-    img = loadImage(imgurlt);
+function preload(){
+    for(let i = 0; i<imgdscr.length; i++){
+        img[i] = loadImage('img/tile'+i+'.jpg');
+    }
 }
 
 function setup(){
     createCanvas(window.innerWidth,window.innerHeight);
     background('#2e445b');
-    font = loadFont('./img/Montserrat-ExtraBold.ttf');
+    font = loadFont('https://mitori.art/img/Montserrat-ExtraBold.ttf');
     textFont(font);
 }
 
 function draw(){
     width = window.innerWidth;
     height = window.innerHeight;
+    noStroke();
     fill('#2e445b');
     rect(0,0,width,height);
-    drawwindow(20,20,100,2,imgurlt,'0xffffff');
-
+    drawwindow(20,20,200,2,0,'0xffffff');
     if (mouseIsPressed == true){
-        drawwindow(20,20,100,2,imgurlt,'#aaaaaa');
+        drawwindow(20,20,200,2,0,'#aaaaaa');
         fill('#cccccc');
         rect(300,0,400,200);
     }
@@ -28,12 +31,13 @@ function draw(){
     
 }
 
-function drawwindow(sx,sy,size,magni,imgurl,color){
+function drawwindow(sx,sy,size,magni,imgnum,color){
     //img = loadImage(imgurl);
     fill(color);
     rect(sx,sy,sx+size*magni,sy+size*magni);
-    img.resize(size*magni,size*magni);
-    image(img,sx+magni*2,sy+magni*2);
+    imageMode(CORNERS)
+    img[imgnum].resize(size*magni*1.1,size*magni*1.1);
+    image(img[imgnum],sx,sy);
 
 }
 
