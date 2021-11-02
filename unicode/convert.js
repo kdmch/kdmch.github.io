2fb2e89371a314bb1ab4c3a84f300a0f120513cb
kdmch <mitori.kadomachi@gmail.com>
2021-11-03 04:45:09 +0900

    unicode

---
+++ unicode/convert.js
@@ -0,0 +1,5 @@
+function encConvert(){
+    var inputValue = "test";
+    inputValue = document.getElementById(enc_textbox).value;
+    document.getElementById(enc_result).innerHTML = inputValue;
+}
