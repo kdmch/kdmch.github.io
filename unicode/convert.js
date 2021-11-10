e7971b3ba93e9e68f7cbd1a1daa86760d344c0f3
kdmch <mitori.kadomachi@gmail.com>
2021-11-11 00:29:23 +0900

    configurated

--- unicode/convert.js
+++ unicode/convert.js
@@ -1,5 +1,17 @@
-function encConvert(){
-    var inputValue = "test";
-    inputValue = document.getElementById(enc_textbox).value;
-    document.getElementById(enc_result).innerHTML = inputValue;
+let text_form = document.getElementById('enc_textbox');
+let output = document.getElementById('emc_result');
+
+timestamp = 0;
+
+function update(){
+	
+	timestamp++;
+	window.requestAnimationFrame(update);
+	
+	if (timestamp % 10 == 0 ){
+		output.innerHTML = {null : text_form.value};
+	}
+	
 }
+
+update();
