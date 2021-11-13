aaf7062c93daa810dba3e851f1936a8e164cfe17
kdmch <mitori.kadomachi@gmail.com>
2021-11-13 18:43:55 +0900

    responsive uw

--- unicode/convert.js
+++ unicode/convert.js
@@ -1,18 +1,21 @@
-const squared = 0x1f130 - 0x41;
-
+const enc　= [0x1f0ef, 0x1f12f, 0x2475, 0x1f10f, 0x1f0cf, 0x1f1a5];
 
 function encTransform($this){
 	let string = $this.value;
+	let element = document.getElementById("enc");
+	let radioGroup = element.encradio;
+	let encType = radioGroup.value;
 	let result = "";
-	let uc, midcp;
+	let converted, character;
+
 	for(let i=0; i<string.length; i++){
-		midcp = string[i].codePointAt(0);
-		if((midcp>0x60)&&(midcp<0x7b)){
-			midcp = midcp - 0x20;
+		character = string[i].codePointAt(0);
+		if((character>0x60)&&(character<0x7b)){
+			character = character - 0x20;
 		}
-		if((midcp>0x40)&&(midcp<0x5b)){
-			uc = squared + midcp;
-			result = result + String.fromCodePoint(uc);
+		if((character>0x40)&&(character<0x5b)){
+			converted = enc[encType] + character;
+			result = result + String.fromCodePoint(converted);
 		}else{
 			result = result + string[i];
 		}
