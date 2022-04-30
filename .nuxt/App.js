aa4424795b70cd4bbdefa79f3e274ce7601222ad
kdmch <mitori.kadomachi@gmail.com>
2022-05-01 02:06:03 +0900

    ビルド忘れてた2

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -4,11 +4,8 @@ import { decode, parsePath, withoutBase, withoutTrailingSlash, normalizeURL } fr
 import { getMatchedComponentsInstances, getChildrenComponentInstancesUsingFetch, promisify, globalHandleError, urlJoin, sanitizeComponent } from './utils'
 import NuxtError from '../layouts/error.vue'
 import NuxtLoading from './components/nuxt-loading.vue'
-import NuxtBuildIndicator from './components/nuxt-build-indicator'
 
 import '../assets/css/globalStyle.css'
-
-import '../node_modules/vuetify/dist/vuetify.css'
 
 import _529c5652 from '../layouts/default-older.vue'
 import _6f6c098b from './layouts/default.vue'
@@ -48,7 +45,7 @@ export default {
       }
     }, [
       loadingEl,
-      h(NuxtBuildIndicator),
+
       transitionEl
     ])
   },
@@ -99,6 +96,10 @@ export default {
 
     isFetching () {
       return this.nbFetching > 0
+    },
+
+    isPreview () {
+      return Boolean(this.$options.previewData)
     },
   },
 
@@ -184,10 +185,6 @@ export default {
     },
 
     setLayout (layout) {
-      if(layout && typeof layout !== 'string') {
-        throw new Error('[nuxt] Avoid using non-string value as layout property.')
-      }
-
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
